5e2b1f21df7f0f4d655ac9056074e6a3276c2619
Dereje <getahun.d@gmail.com>
2021-06-13 20:55:39 -0400

    Upgrade router

--- src/client.js
+++ src/client.js
@@ -1,50 +1,51 @@
-"use strict" //primary module that ties store actions with reducers
+"use strict"; //primary module that ties store actions with reducers
 
 //redux modules
-import {applyMiddleware, createStore} from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 //react modules
-import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router, Route, IndexRoute,browserHistory} from 'react-router';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 //Import all Created react components that are associated with the router
 //standard menu (non-authenticated) components
-import Main from './main'
-import Home from './components/home'
-import About from './components/about'
-import Signup from './components/authentication/signup'
-import Login from './components/authentication/login'
+import Menu from './components/menu'
+import Home from "./components/home";
+import About from "./components/about";
+import Signup from "./components/authentication/signup";
+import Login from "./components/authentication/login";
 //componets visible only after logged in
-import Profile from './components/authentication/profile'
-import Books from './components/books'
+import Profile from "./components/authentication/profile";
+import Books from "./components/books";
 //import combined reducer to pass to store here
-import reducers from './reducers/index'
+import reducers from "./reducers/index";
 
 //use logger for debugging only
 //const middleware = applyMiddleware(thunk,logger)
-const middleware = applyMiddleware(thunk)
-const store = createStore(reducers,middleware)
+const middleware = applyMiddleware(thunk);
+const store = createStore(reducers, middleware);
 
 //decalre all routes of application below note that /profileadd,/passchange ,/mybooks actually
 //do not have a link going to them , instead they are included in the routes for serving
 //authneticating/authenticated actions
 const Routes = (
-<Provider store={store}>
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={Home}/>
-      <Route path="/signup" component={Signup}/>
-      <Route path="/login" component={Login}/>
-      <Route path="/profileadd" component={Profile}/>
-      <Route path="/passchange" component={Profile}/>
-      <Route path="/mybooks" component={Books}/>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
-</Provider>
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/profileadd" component={Profile} />
+        <Route path="/passchange" component={Profile} />
+        <Route path="/mybooks" component={Books} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+);
 // render routes
-render (Routes,document.getElementById('app'))
+render(Routes, document.getElementById("app"));
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,54 +1,82 @@
-"use strict"//navigation bar self explanatory
-import React from 'react'
-import {connect} from 'react-redux'
-import {Nav, NavItem, Navbar, Button} from 'react-bootstrap';
+"use strict"; //navigation bar self explanatory
+import React from "react";
+import { connect } from "react-redux";
+import { Nav, NavItem, Navbar, Button } from "react-bootstrap";
+import { getUser } from "../actions/authentication";
 
-class Menu extends React.Component{
-  conditionalNav(){
-    let autenticationStatus = this.props.user.user.authenticated
-    if(autenticationStatus){//the way response comes of user is in string I can change this to JSON response in the future
-      return(
-        <Nav pullRight>
-          <NavItem eventKey={2} href="/">Home</NavItem>
-          <NavItem eventKey={3} href="/mybooks">My Books</NavItem>
-          <NavItem eventKey={4} href="/profileadd">Profile</NavItem>
-          <NavItem eventKey={5} href="/logout">Logout @ {this.props.user.user.userName}</NavItem>
-        </Nav>
-      )
-    }
-    else{
+const actionCreators = {
+  getUser,
+};
+
+class Menu extends React.Component {
+  componentDidMount() {
+    console.log("CDM Mounted for Menu");
+    this.props.getUser();
+  }
+
+  conditionalNav() {
+    let autenticationStatus = this.props.user.user.authenticated;
+    if (autenticationStatus) {
+      //the way response comes of user is in string I can change this to JSON response in the future
       return (
         <Nav pullRight>
-          <NavItem eventKey={2} href="/">Home</NavItem>
-          <NavItem eventKey={4} href="/login">Login</NavItem>
-          <NavItem eventKey={3} href="/signup">Sign Up</NavItem>
+          <NavItem eventKey={2} href="/">
+            Home
+          </NavItem>
+          <NavItem eventKey={3} href="/mybooks">
+            My Books
+          </NavItem>
+          <NavItem eventKey={4} href="/profileadd">
+            Profile
+          </NavItem>
+          <NavItem eventKey={5} href="/auth/logout">
+            Logout @ {this.props.user.user.userName}
+          </NavItem>
         </Nav>
-      )
+      );
+    } else {
+      return (
+        <Nav pullRight>
+          <NavItem eventKey={2} href="/">
+            Home
+          </NavItem>
+          <NavItem eventKey={4} href="/login">
+            Login
+          </NavItem>
+          <NavItem eventKey={3} href="/signup">
+            Sign Up
+          </NavItem>
+        </Nav>
+      );
     }
   }
 
-  render(){
-    let bcolor = (this.props.user.user.authenticated) ? "#7ef74a" : "white"
-    return(
-    <Navbar fixedTop>
+  render() {
+    let bcolor = this.props.user.user.authenticated ? "#7ef74a" : "white";
+    return (
+      <Navbar fixedTop>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="/" style={{"color":bcolor}}>Book Trading App</a>
+            <a href="/" style={{ color: bcolor }}>
+              Book Trading App
+            </a>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href="/about">About</NavItem>
+            <NavItem eventKey={1} href="/about">
+              About
+            </NavItem>
           </Nav>
           {this.conditionalNav()}
         </Navbar.Collapse>
-    </Navbar>
-    )
+      </Navbar>
+    );
   }
 }
-function mapStateToProps(state){
-  return state
+function mapStateToProps(state) {
+  return state;
 }
 //only reads store state does not write to it
-export default connect(mapStateToProps)(Menu)
+export default connect(mapStateToProps, actionCreators)(Menu);
